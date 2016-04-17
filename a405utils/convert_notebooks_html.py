aadef99d89233bf667f86c36546be7f95eff2cf0
Phil Austin <paustin@eos.ubc.ca>
2016-04-16 20:18:35 -0700

    final checkin

--- a405utils/convert_notebooks_html.py
+++ a405utils/convert_notebooks_html.py
@@ -5,7 +5,7 @@
 
    usage:
 
-   ./convert_notebooks_html.py  notebook_folder
+   ./convert_notebooks.py  -f notebook_folder -t html
 
    result:
 
@@ -58,70 +58,95 @@ if __name__ == "__main__":
     descrip=textwrap.dedent(globals()['__doc__'])
     parser = argparse.ArgumentParser(formatter_class=linebreaks,description=descrip)
     parser.add_argument('--folder','-f',type=str,help='folder containing ipynb files (default current directory)',default='.')
+    parser.add_argument('--type','-f',type=str,help='pdf or html',default='html')
     args=parser.parse_args()
     try:
         currdir=os.getcwd()
         os.chdir(args.folder)
-        #
-        # make the html folder if it doesn't exist
-        #
-        html_files='html'
-        if not os.path.exists(html_files):
-            print('creating html folder')
-            os.makedirs(html_files)
+        if args.type not in ['html','pdf']:
+            raise ValueError('type needs to be html or pdf')
+        output_dir=args.type
+        if not os.path.exists(output_dir):
+            print('creating output folder')
+            os.makedirs(output_dir)
 
         #
         # get the ipynb and py files
         #
         notebooklist=glob.glob('*.ipynb')
-        htmllist=glob.glob('./html/*.html')
+        outlist=glob.glob('{}/*.{}')
         #
         #  build two dictionaries containing the root names and the modtimes
         #
-        html_dict={}
+        out_dict={}
         nb_dict={}
         for the_file in notebooklist:
             head, the_date = find_modtime(the_file)
             nb_dict[head] = the_date
 
-        for the_file in htmllist:
+        for the_file in outlist:
             head, the_date = find_modtime(the_file)
-            html_dict[head] = the_date
+            out_dict[head] = the_date
 
         # use  sets to
-        #find notebooks not in htmllist
+        #find notebooks not in outlist
         #
-
-        html_files=set(html_dict.keys())
+u
+        out_files=set(out_dict.keys())
         nb_files=set(nb_dict.keys())
-        print('{html_files!r}'.format_map(vars()))
+        print('{out_files!r}'.format_map(vars()))
         #
         # find all notebooks that don't have py files
         #
-        make_py=nb_files - html_files
+        make_py=nb_files - out_files
 
         #
-        #  run ipython nbconvert on these notebook and put output in python folder
+        #  run jupyter nbconvert on these notebook and put output in python folder
         #
         print('rebuilding {}'.format(make_py))
-        cmdstring='ipython nbconvert --stdout --to html {0:s}.ipynb > html/{0:s}.html'
-        for the_file in make_py:
-            command=cmdstring.format(the_file)
-            print('executing: ',command)
-            out=subprocess.getstatusoutput(command)
-            print(out)
+        if args.type == 'html':
+            cmdstring='jupyter nbconvert --to html {0:s}.ipynb --output-dir html'
+            for the_file in make_py:
+                command=cmdstring.format(the_file)
+                print('executing: ',command)
+                out=subprocess.getstatusoutput(command)
+                print(out)
+        elif args.type == 'pdf':
+            cmdstring='jupyter nbconvert --to latex --output-dir pdf {0:s}.ipynb'
+            for the_file in make_py:
+                command=cmdstring.format(the_file)
+                print('executing: ',command)
+                out=subprocess.getstatusoutput(command)
+                print(out)
+                try:
+                    orig_dir = os.getcwd()
+                    os.chdir('pdf')
+                    cmdstring='latexmk -pdf {0:s}.tex'
+                    command=cmdstring.format(the_file)
+                    print('executing: ',command)
+                    out=subprocess.getstatusoutput(command)
+                    print(out)
+                except Exception as e:
+                    ex_type, ex_val, tb = sys.exc_info()
+                    print('bummer: ',ex_val)
+                    print('\nhere is the traceback:\n')
+                    traceback.print_tb(tb)
+                finally:
+                    os.chdir(orig_dir)
+        else:
+            raise ValueError('type needs to be html or pdf')
         #
         # now go through notebooks once more, converting all that are newer than their html files
         #
-        htmllist=glob.glob('./html/*.html')
-        for the_file in htmllist:
+        outlist=glob.glob('./html/*.html')
+        for the_file in outlist:
             head, the_date = find_modtime(the_file)
-            html_dict[head] = the_date
+            out_dict[head] = the_date
 
         for the_root in nb_files:
             nb='{}.ipynb'.format(the_root)
             py='html/{}.html'.format(the_root)
-            if nb_dict[the_root] > html_dict[the_root]:
+            if nb_dict[the_root] > out_dict[the_root]:
                 print('rebuilding {}'.format(the_root))
                 command=cmdstring.format(the_root)
                 out=subprocess.getstatusoutput(command)
